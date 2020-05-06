7d281febef01a3b9d67cfef515639c270c472edb
Liam <liamtrnr80@gmail.com>
2020-05-06 23:13:02 +1000

    updated slideshow

--- index.php
+++ index.php
@@ -18,21 +18,33 @@ session_start();
     <?php include_once('includes/nav.php'); ?>
     
     <div class="uk-container full-height">
-        <div uk-slideshow>
-            <ul class="uk-slideshow-items">
-                <li>
-                    <img src="img/Main1.jpeg" alt="" uk-cover>
-                </li>
-                <li>
-                    <img src="img/Main2.jpeg" alt="" uk-cover>
-                </li>
-                <li>
-                    <img src="img/Main3.jpeg" alt="" uk-cover>
-                </li>
-            </ul>
-            
-            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
-            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+        <div class="uk-container">
+            <div class="uk-position-relative uk-light" uk-slideshow="autoplay: true">
+                <ul class="uk-slideshow-items">
+                    <li>
+                        <img src="img/Main1.jpeg" alt="" uk-cover>
+                    </li>
+                    <li>
+                        <img src="img/Main2.jpeg" alt="" uk-cover>
+                    </li>
+                    <li>
+                        <img src="img/Main3.jpeg" alt="" uk-cover>
+                    </li>
+                </ul>
+
+                <div class="uk-position-bottom-center uk-postion-small">
+                    <ul class="uk-dotnav">
+                        <li uk-slideshow-item="0"><a href="#">Item 1</a></li>
+                        <li uk-slideshow-item="1"><a href="#">Item 2</a></li>
+                        <li uk-slideshow-item="2"><a href="#">Item 3</a></li>
+                    </ul>
+                </div>
+                <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+                <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+            </div>
+        </div>
+        <div class="container">
+
         </div>
     </div>
 
