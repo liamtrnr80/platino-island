fdfa904c30c5a684c8045c058e010f87ea0e0f60
Liam <liamtrnr80@gmail.com>
2020-05-06 16:09:24 +1000

    added template to contact-us and shop

---
+++ contact-us.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Us</title>
+    <?php include_once('includes/header.php'); ?>
+</head>
+
+<body>
+    <?php include_once('includes/nav.php'); ?>
+    
+
+    <footer>
+        <?php include_once('includes/footer.php'); ?>
+    </footer>
+</body>
--- shop.php
+++ shop.php
@@ -1,3 +1,19 @@
 <?php
 session_start();
 ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shop</title>
+    <?php include_once('includes/header.php'); ?>
+</head>
+
+<body>
+    <?php include_once('includes/nav.php'); ?>
+
+    <footer>
+        <?php include_once('includes/footer.php'); ?>
+    </footer>
+</body>
