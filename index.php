6d385327ed819ee7a1a256b0741d215e32fa24ba
Liam <liamtrnr80@gmail.com>
2020-05-06 22:57:16 +1000

    work of index

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ session_start();
                     <img src="img/Main3.jpeg" alt="" uk-cover>
                 </li>
             </ul>
+            
             <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
             <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
         </div>
