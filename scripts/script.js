2221e75a46f827793cdafa177971d32ee1c24be5
Liam <liamtrnr80@gmail.com>
2020-05-19 00:11:00 +1000

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,41 @@
 simpleCart({
-    cartStyle: "table",
-    currency: "AUD",
-    
+    cartColums: [
+        { attr: "name", label: "Name" },
+        { attr: "price", label: "Price", view: 'currency'},
+        { view: "decrement", label: false },
+        { attr: "quantity", label: "Qty" },
+        { view: "increment", label: false },
+        { attr: "total", label: "SubTotal", view: 'currency' },
+        { view: "remove", text: "Remove", label: false }
+    ],
+
+    cartStyle: "div",
+
     checkout: {
-        type: "Paypal",
+        type: "PayPal",
         email: "you@yours.com"
-    }
+    },
+
+    currency: "AUD",
+
+    data: {},
+
+    language: "english-us",
+
+    excludeFromCheckout: [],
+
+    shippingFlatRate: 0,
+    taxRate: 0,
+    
+    // Event Callbacks
+    beforeAdd: null,
+    afterAdd: null,
+    load: null,
+    beforeSave: null,
+    afterSave: null,
+    update: null,
+    ready: null,
+    checkoutSuccess: null,
+    checkoutFail: null,
+    beforeCheckout: null
 })
